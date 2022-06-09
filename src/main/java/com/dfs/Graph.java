0235426a88e85e6d4978803382ce1fdc35967677
zephyrpawan <zephyrpawan@gmail.com>
2022-06-08 20:54:05 -0500

    DFS implementation by recursion & iteration

--- src/main/java/com/dfs/Graph.java
+++ src/main/java/com/dfs/Graph.java
@@ -1,6 +1,8 @@
 package com.dfs;
 
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class Graph {
 	private int vertices;
@@ -11,21 +13,63 @@ public class Graph {
 		this.vertices = vertices;
 		adjList = new LinkedList[vertices];
 		for (int i = 0; i < vertices; ++i) {
-			adjList[i] = new LinkedList<Integer>();
+			adjList[i] = new LinkedList<>();
 		}
 	}
-	
-	//method to add an edge to a graph(adjacency list)
+
+	// method to add an edge to a graph(adjacency list)
 	void addEdge(int v, int w) {
 		adjList[v].add(w);
 	}
 
-	public int getVertices() {
-		return vertices;
+	void DFSTraverseByRecursion(int vertex, boolean visited[]) {
+		// mark the current node as visited and print it
+		visited[vertex] = true;
+		System.out.println(vertex + " ");
+
+		// move to next node/vertex and recurse for all vertices
+		Iterator<Integer> itr = adjList[vertex].listIterator();
+		while (itr.hasNext()) {
+			int next = itr.next();
+			if (!visited[next]) {
+				DFSTraverseByRecursion(next, visited);
+			}
+		}
 	}
 
-	public LinkedList<Integer>[] getAdjList() {
-		return adjList;
+	void DFSByRecursion(int vertex) {
+		// Initialize all vertices as not visited
+		boolean visited[] = new boolean[vertices];
+
+		DFSTraverseByRecursion(vertex, visited);
 	}
 
+	void DFSByIteration(int vertex) {
+		boolean visited[] = new boolean[vertices];
+
+		// initialize a stack and push the root node to the stack
+		Stack<Integer> stack = new Stack<>();
+		stack.push(vertex);
+
+		int nextNode = 0;
+
+		while (!stack.empty()) {
+			vertex = stack.peek();
+			stack.pop();
+
+			if (visited[vertex] == false) {
+				System.out.println(vertex + " ");
+				visited[vertex] = true;
+			}
+
+			Iterator<Integer> itr = adjList[vertex].listIterator();
+			while (itr.hasNext()) {
+				nextNode = itr.next();
+				if (!visited[nextNode]) {
+					stack.push(nextNode);
+				}
+			}
+		}
+
+	}
 }
--- src/main/java/com/dfs/main.java
+++ src/main/java/com/dfs/main.java
@@ -5,7 +5,17 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Testing DFS");
+		Graph graph  = new Graph(4);
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 2);
+		graph.addEdge(1, 2);
+		graph.addEdge(2, 3);
+		graph.addEdge(2, 0);
+		graph.addEdge(3, 3);
+		
+		graph.DFSByRecursion(1);
+		System.out.println('\n');
+		graph.DFSByIteration(2);
 
 	}
 
